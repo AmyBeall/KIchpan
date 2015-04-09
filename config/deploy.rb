49e7ed52e49902a30e30d8f5b001ab15ede6f08d
AmyBeall <amylouisebeall@gmail.com>
2015-04-08 20:06:12 -0700

    took off symlink

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,13 +22,13 @@ namespace :deploy do
 
 end
 
-namespace :images do
-  task :symlink, :except => { :no_release => true } do
-    run "rm -rf #{release_path}/public/spree"
-    run "ln -nfs #{shared_path}/spree #{release_path}/public/spree"
-  end
-end
-after "bundle:install", "images:symlink"
+# namespace :images do
+#   task :symlink, :except => { :no_release => true } do
+#     run "rm -rf #{release_path}/public/spree"
+#     run "ln -nfs #{shared_path}/spree #{release_path}/public/spree"
+#   end
+# end
+# after "bundle:install", "images:symlink"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp

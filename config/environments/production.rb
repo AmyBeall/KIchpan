485c0bf26962c3aa785392de9dadac74738aa925
AmyBeall <amylouisebeall@gmail.com>
2015-04-16 10:12:49 -0700

    fix glyph v.2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+
+  config.assets.precompile += %w(FontAwesome.otf fontawesome-webfont.eot fontawesome-webfont.svg fontawesome-webfont.ttf fontawesome-webfont.woff)
 
   # Generate digests for assets URLs.
   config.assets.digest = true

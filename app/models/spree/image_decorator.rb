6d8075afec21dbd1f8916fbfc381abc739778513
AmyBeall <amylouisebeall@gmail.com>
2015-04-17 17:31:58 -0700

    fixed typo

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,6 +1,6 @@
- Spree::Taxon.class_eval do
+ Spree::Image.class_eval do
    
-    Spree::Taxon.attachment_definitions[:attachment][:styles] = { 
+    Spree::Image.attachment_definitions[:attachment][:styles] = { 
     	mini: '48x48#>', 
     	small: '100x100#>', 
     	product: '240x240#>', 
